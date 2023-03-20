5946802d753bdb81aa2aa2a2f8cff11a95be3d8e
Javier Cañadillas <javiercm@google.com>
2023-03-20 20:15:52 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Implementing Cloud Native Principles with Cloud Run
+
+This is lab to be followed with the Qwiklabs platform. Ask your instructor for these two assets:
+
+- Lab URL
+- Lab Document
---
+++ vars.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# vars.bash: simple environment configuration script
+# run it like this: "source vars.sh"
+
+declare -x PROJECT_ID
+PROJECT_ID=$(gcloud config get-value project)
+declare -x REGION=europe-west
+declare -x AE_REGION=europe-west1
+
+# set the metrics-writer URL, ignoring if not present
+declare -x WRITER_URL
+WRITER_URL="$(gcloud run services describe metrics-writer --format='value(status.url)' 2> /dev/null)"
+
+# set the Cloud Run (fully managed) region
+gcloud config set run/region $REGION
