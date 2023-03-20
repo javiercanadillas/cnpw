c8d63b297aa9d58be809690c15c0a1932b3bf2f3
Javier Cañadillas <javiercm@google.com>
2023-03-20 22:55:15 +0100

    Mode writer_url to a function

--- .labenv_custom.bash
+++ .labenv_custom.bash
@@ -6,9 +6,10 @@
 declare -x AE_REGION=europe-west
 
 # set the metrics-writer URL, ignoring if not present
-declare -x WRITER_URL
-WRITER_URL="$(gcloud run services describe metrics-writer --format='value(status.url)' 2> /dev/null)"
-
+get_writer_url() {
+  declare -x WRITER_URL
+  WRITER_URL="$(gcloud run services describe metrics-writer --format='value(status.url)' 2> /dev/null)"
+}
 # set the working directory
 declare -x WORKDIR="$HOME/cnpw"
 
