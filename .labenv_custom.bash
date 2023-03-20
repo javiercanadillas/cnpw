ef15ba26a96c0d173582a0ba5ad0f407653a7251
Javier Cañadillas <javiercm@google.com>
2023-03-20 22:08:25 +0100

    Fix AE region

--- .labenv_custom.bash
+++ .labenv_custom.bash
@@ -3,7 +3,7 @@
 ## .labven_custom.bash: This script is intended to be sourced, additional environment customizations
 
 # Set up 
-declare -x AE_REGION=europe-west1
+declare -x AE_REGION=europe-west
 
 # set the metrics-writer URL, ignoring if not present
 declare -x WRITER_URL
@@ -13,4 +13,9 @@ WRITER_URL="$(gcloud run services describe metrics-writer --format='value(status
 declare -x WORKDIR="$HOME/cnpw"
 
 # set the Cloud Run (fully managed) region
-gcloud config set run/region "$REGION" 2>/dev/null
+echo "Setting Cloud Run region to $REGION..."
+gcloud config set run/region "$REGION"
+
+# set Cloud Run managed platform
+echo "Setting Cloud Run platform to managed"
+gcloud config set run/platform managed 2>/dev/null
