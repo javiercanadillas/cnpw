959e95ce07aacd80e7065f198b8fe38f8c8c3361
Javier Cañadillas <javiercm@google.com>
2023-05-17 15:51:42 +0100

    Adding Imgs source URLs

--- .labenv_custom.bash
+++ .labenv_custom.bash
@@ -3,7 +3,7 @@
 ## .labven_custom.bash: This script is intended to be sourced, additional environment customizations
 
 # Set up 
-declare -x AE_REGION=europe-west
+declare -x AE_REGION="europe-west"
 
 # set the metrics-writer URL, ignoring if not present
 get_writer_url() {
@@ -13,10 +13,14 @@ get_writer_url() {
 # set the working directory
 declare -x WORKDIR="$HOME/cnpw"
 
+# Set the source images to use
+declare -x MW_IMG="europe-southwest1-docker.pkg.dev/javiercm-main-dev/public/metrics-writer:latest"
+declare -x V_IMG="europe-southwest1-docker.pkg.dev/javiercm-main-dev/public/visualizer:latest "
+
 # set the Cloud Run (fully managed) region
 echo "Setting Cloud Run region to $REGION..."
-gcloud config set run/region "$REGION"
+gcloud config set run/region "$REGION" --no-user-output-enabled
 
 # set Cloud Run managed platform
 echo "Setting Cloud Run platform to managed"
-gcloud config set run/platform managed 2>/dev/null
+gcloud config set run/platform managed --no-user-output-enabled
